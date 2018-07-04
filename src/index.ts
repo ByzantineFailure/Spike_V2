20045888c817c60a9c6349862716642553a00fa5
ByzantineFailure <byzanitnefailure@gmail.com>
2018-07-04 14:38:50 -0700

    Working DB, walk directory tree

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,40 @@
 import {initializeDatabase} from './lib/db/init';
+import {walk_tree} from './lib/fs/walk_tree';
+import {Database} from 'sqlite3';
 
-initializeDatabase().then(() => {
+import {Stats} from 'fs';
+
+let db: Database;
+
+async function main() {
+    console.log('initializing database...');
+    db = await initializeDatabase();
     console.log('initialized!');
+    await walk_tree('./src', 
+        dir,
+        file,
+    );
+}
+
+function dir(path: string, stats: Stats): Promise<void> {
+    return new Promise(res => {
+        if(stats) {
+            console.log('dir: ' + path);
+        }
+        res();
+    });
+}
+
+function file(path: string): Promise<void> {
+    return new Promise(res => {
+        console.log('file: ' + path);
+        res();
+    });
+}
+
+main().then(() => { 
+    db.close(); 
+}).catch(err => {
+    console.log(err);
+    db.close();
 });
--- src/lib/db/init.ts
+++ src/lib/db/init.ts
@@ -2,13 +2,11 @@ import * as sqlite3 from 'sqlite3';
 
 const sqlite = sqlite3.verbose();
 
-export async function initializeDatabase(path: string = '.'): Promise<sqlite3.Database> {
+export async function initializeDatabase(path: string = './.spike.db'): Promise<sqlite3.Database> {
     const database = await connect(path);
-    
-    createSeriesTable(database);
-    createSeasonTable(database);
-    createEpisodeTable(database);
-
+    await createSeriesTable(database);
+    await createSeasonTable(database);
+    await createEpisodeTable(database);
     return database;
 }
 
@@ -16,7 +14,7 @@ function connect(path: string): Promise<sqlite3.Database> {
    return new Promise((res, rej) => {
     const db = new sqlite.Database(path, (err) => {
         if (err) {
-            rej(`Error connecting to sqlite database at path ${path}`);
+            rej(err);
             return;
         }
 
@@ -26,7 +24,7 @@ function connect(path: string): Promise<sqlite3.Database> {
    });
 }
 
-function createSeriesTable(db: sqlite3.Database): void {
+function createSeriesTable(db: sqlite3.Database): Promise<void> {
    const query = `
      CREATE TABLE IF NOT EXISTS series (
        id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,10 +32,10 @@ function createSeriesTable(db: sqlite3.Database): void {
        path TEXT NOT NULL
      );
    `;
-    db.run(query);
+    return runQuery(db, query);
 }
 
-function createSeasonTable(db: sqlite3.Database): void {
+function createSeasonTable(db: sqlite3.Database): Promise<void> {
     const query = `
        CREATE TABLE IF NOT EXISTS season (
          id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -47,21 +45,30 @@ function createSeasonTable(db: sqlite3.Database): void {
          FOREIGN KEY(series) REFERENCES series(id) ON DELETE CASCADE
        );
     `;
-
-    db.run(query);
+    return runQuery(db, query);
 }
 
-function createEpisodeTable(db: sqlite3.Database): void {
+function createEpisodeTable(db: sqlite3.Database): Promise<void> {
     const query = `
       CREATE TABLE IF NOT EXISTS episode (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         path TEXT NOT NULL,
         series INTEGER NOT NULL,
-        season INTEGER
+        season INTEGER,
         FOREIGN KEY(series) REFERENCES series(id) ON DELETE CASCADE,
-        FOREIGN KEY(season) REFERENCES season(id) ON DELETE CASCADE
+        FOREIGN KEY(season) REFERENCES season(id) ON DELETE CASCADE);
     `;
+    return runQuery(db, query);
+}
 
-    db.run(query);
+function runQuery(db: sqlite3.Database, query: string): Promise<void> {
+    return new Promise((res, rej) => {
+        db.run(query, (err) => {
+            if(err) {
+                rej(err);
+            }
+            res();
+        });
+    });
 }
